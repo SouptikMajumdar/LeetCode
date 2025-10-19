9ecac0a9ee460af8d369d69df7eb23ee7262a848
Souptik Majumdar <32878956+SouptikMajumdar@users.noreply.github.com>
2025-10-19 12:54:06 +0530

    Time: 478 ms (14.92%), Space: 18.1 MB (87.87%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -26,23 +26,5 @@ class Solution:
         return traverse(root)
 
 
-#class Solution:
-  #  def pathSum(self, root, targetSum):
-   #     def countPathsFrom(node, current_sum):
-       #     if not node:
-      #          return 0
-      #      current_sum += node.val
-      #      count = 1 if current_sum == targetSum else 0
-       #     count += countPathsFrom(node.left, current_sum)
-     #       count += countPathsFrom(node.right, current_sum)
-      #      return count
 
-      #  def traverse(node):
-     #       if not node:
-    #            return 0
-       #     return (countPathsFrom(node, 0) +
-       #             traverse(node.left) +
-       #             traverse(node.right))
-
-      #  return traverse(root)
 
