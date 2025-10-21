13b9d0e6319f1b908e5090a2dece748eab3815f4
Souptik Majumdar <32878956+SouptikMajumdar@users.noreply.github.com>
2025-10-21 22:07:02 +0530

    Time: 43 ms (13.29%), Space: 21.6 MB (76.22%) - LeetHub

---
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        
+        depthSum = {1: 0}
+        def dfs(node, depth):
+            if not node:
+                return
+            
+            depthSum[depth] = depthSum.get(depth, 0) + node.val
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+            
+            depth -= 1
+            return
+        
+        dfs(root, 1)
+
+        maxVal = max(depthSum.values())
+        minDepth = min(list(filter(lambda key: depthSum[key] == maxVal, depthSum)))
+
+        print(depthSum)
+
+        return minDepth
+
+
+            
+
+
+            
+            
