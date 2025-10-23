76d57c7eaa44b8f0972b0d4f2a0639ae6667b6ad
Souptik Majumdar <32878956+SouptikMajumdar@users.noreply.github.com>
2025-10-23 14:04:34 +0530

    Time: 103 ms (5.07%), Space: 18.3 MB (29.92%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        currKeys = {0: 0, -1: -1}
+        toVisit = 0
+        while len(set(currKeys.values())) != 1:
+            if -1 in currKeys:
+                currKeys.pop(-1)
+
+            newKeys = rooms[toVisit]
+            if len(currKeys.keys()) == len(rooms):
+                break
+            
+            for key in newKeys:
+                if key not in currKeys: 
+                    currKeys[key] = 0
+            
+            currKeys[toVisit] = 1
+            toVisit = list(filter(lambda key: currKeys[key] == 0, currKeys))
+            if len(toVisit) != 0:
+                toVisit = toVisit[0]
+            else:
+                break
+            
+        print(currKeys)
+        return len(currKeys) == len(rooms)
