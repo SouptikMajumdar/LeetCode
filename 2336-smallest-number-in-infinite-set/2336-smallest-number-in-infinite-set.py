21d0fcf289941c6a7ad2de7f574c04dabf9d924b
Souptik Majumdar <32878956+SouptikMajumdar@users.noreply.github.com>
2026-01-15 12:02:11 +0100

    Time: 21 ms (52.3%), Space: 20.1 MB (12%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -1,10 +1,34 @@
 class SmallestInfiniteSet:
 
     def __init__(self):
-        self.removed = []
+        self.minHeap = []
+        self.addedBack = set()
         self.smallest = 1
 
-    def find_smallest(self) -> int:
+
+
+    def popSmallest(self) -> int:
+        if self.minHeap:
+            smallest = heapq.heappop(self.minHeap)
+            self.addedBack.remove(smallest)
+            return smallest
+        ret = self.smallest
+        self.smallest += 1
+        return ret
+
+        """ret = self.smallest
+        self.removed.append(ret)
+        self.removed.sort()
+        self.smallest = self.find_smallest()
+        return ret """
+
+    def addBack(self, num: int) -> None:
+        if num < self.smallest and num not in self.addedBack:
+            heapq.heappush(self.minHeap, num)
+            self.addedBack.add(num)
+
+
+"""     def find_smallest(self) -> int:
         if len(self.removed) == 0:
             return -1
         if len(self.removed) == 1:
@@ -12,21 +36,7 @@ class SmallestInfiniteSet:
         for i in range(0,len(self.removed)-1):
             if self.removed[i]+1 != self.removed[i+1]:
                 return self.removed[i] + 1
-        return self.removed[-1] + 1
-
-    def popSmallest(self) -> int:
-        ret = self.smallest
-        self.removed.append(ret)
-        self.removed.sort()
-        self.smallest = self.find_smallest()
-        return ret
-
-    def addBack(self, num: int) -> None:
-        if num in self.removed:
-            self.removed.remove(num)
-            if num < self.smallest:
-                self.smallest = num
-
+        return self.removed[-1] + 1 """
 
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:
